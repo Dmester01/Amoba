0a8704e9f8a3952cda467263c3e0aae79cbb9105
Dmester <majordani0121@gmail.com>
2021-05-25 13:28:06 +0200

    folytatás

--- Javascript.js
+++ Javascript.js
@@ -2,14 +2,32 @@
 
 
 function palyarajzol(){
+    
+    $palyameret=[9,25,110];
+    $kirajzolni=0;
+    for (var i = 0; i < palyameret[i]; i++) {
+        i=$kirajzolni
+       
+    }
+    
+    for (var j = 0; j <=kirajzolni ; j++) {
+      if("click",id="ok3")
+          ok3.addEventListener("click");
+      if("click",id="ok5")
+          ok5.addEventListener("click");
+      else
+         ok10.addEventListener("click");
+    }
    
+    
+  
     
    
 }
 
 
 function ujbutton(){
-    
+    ujjatek.click();
     
     
 }
